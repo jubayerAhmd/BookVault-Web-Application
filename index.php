a01c35271277bf22fe814a57c69d699c77f0406c
MD RAKIB CHOWDHURY <135139732+rakibCHD@users.noreply.github.com>
2025-03-19 15:55:12 +0600

    Update index.php

--- index.php
+++ index.php
@@ -4,13 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My First Webpage</title>
+    <link rel="stylesheet" href="CSS/style.css">
 </head>
 <body>
-    
-<header>
-    <h1>Welcome to The BookVault</h1>
-    <p>You can search for Book.</p>
-</header>
-    
+
+    <header>
+
+        <h1>::::Welcome to BookVault::::</h1>
+        <h3>.There are many books in BookVault.</h3>
+
+    </header>
+
+
+
 </body>
 </html>
