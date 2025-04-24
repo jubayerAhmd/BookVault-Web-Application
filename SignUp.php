fd7635c0fdc17f2c8d94f9c3a13533e8c5856527
MD RAKIB CHOWDHURY <135139732+rakibCHD@users.noreply.github.com>
2025-04-25 03:44:55 +0600

    Update SignUp.php

--- SignUp.php
+++ SignUp.php
@@ -3,71 +3,80 @@
 <head>
     <title>User SignUp</title>
     <link rel="stylesheet" href="./CSS/SignUpStyle.css">
-
+    <script src="./JS/SignUpValidation.js" defer></script>
+    <script src="./JS/SignUpToLogin.js" defer></script>
 </head>
 <body>
 
 <header class="header">
-    <?php
-    echo "BookVault " 
-    ?>
+    <?php echo "BookVault"; ?>
 </header>
 
-    <h2>User Registration Form</h2>
-    <form>
-        <table border="1" cellpadding="5" cellspacing="0">
-            <tr>
-                <td>Full Name:</td>
-                <td><input type="text" name="fullname" required></td>
-            </tr>
-            <tr>
-                <td>Email:</td>
-                <td><input type="email" name="email" required></td>
-            </tr>
-            <tr>
-                <td>Password:</td>
-                <td><input type="password" name="password" required></td>
-            </tr>
-            <tr>
-                <td>Gender:</td>
-                <td>
-                    <input type="radio" name="gender" value="Male"> Male
-                    <input type="radio" name="gender" value="Female"> Female
-                </td>
-            </tr>
-            <tr>
-                <td>Country:</td>
-                <td>
-                    <select name="country">
-                        <option value="USA">USA</option>
-                        <option value="UK">UK</option>
-                        <option value="India">India</option>
-                        <option value="Canada">Canada</option>
-                        <option value="Pakistan">Pakistan</option>
-                        <option value="Japan">Japan</option>
-                        <option value="Bangladesh">Bangladesh</option>
-                    </select>
-                </td>
-            </tr>
-            <tr>
-                <td>Hobbies:</td>
-                <td>
-                    <input type="checkbox" name="hobbies[]" value="Reading"> Reading
-                    <input type="checkbox" name="hobbies[]" value="Sports"> Sports
-                    <input type="checkbox" name="hobbies[]" value="Music"> Music
-                </td>
-            </tr>
-            <tr>
-                <td>Profile Picture:</td>
-                <td><input type="file" name="profile_pic"></td>
-            </tr>
-            <tr>
-                <td colspan="2" align="center">
-                    <input type="submit" value="Register">
-                    <input type="reset" value="Clear">
-                </td>
-            </tr>
-        </table>
-    </form>
+<h2>User Registration Form</h2>
+
+<form id="signupForm" onsubmit="return validateForm()" method="POST" enctype="multipart/form-data">
+    <table border="1" cellpadding="5" cellspacing="0">
+        <tr>
+            <td>Full Name:</td>
+            <td>
+                <input type="text" id="fullname" name="fullname">
+                <span id="nameError" style="color:red;"></span>
+            </td>
+        </tr>
+        <tr>
+            <td>Email:</td>
+            <td>
+                <input type="email" id="email" name="email">
+                <span id="emailError" style="color:red;"></span>
+            </td>
+        </tr>
+        <tr>
+            <td>Password:</td>
+            <td>
+                <input type="password" id="password" name="password">
+                <span id="passwordError" style="color:red;"></span>
+            </td>
+        </tr>
+        <tr>
+            <td>Gender:</td>
+            <td>
+                <input type="radio" name="gender" value="Male"> Male
+                <input type="radio" name="gender" value="Female"> Female
+                <span id="genderError" style="color:red;"></span>
+            </td>
+        </tr>
+        <tr>
+            <td>Country:</td>
+            <td>
+                <select name="country">
+                    <option value="">--Select--</option>
+                    <option value="USA">USA</option>
+                    <option value="UK">UK</option>
+                    <option value="India">India</option>
+                    <option value="Canada">Canada</option>
+                    <option value="Pakistan">Pakistan</option>
+                    <option value="Japan">Japan</option>
+                    <option value="Bangladesh">Bangladesh</option>
+                </select>
+            </td>
+        </tr>
+        <tr>
+            <td>Hobbies:</td>
+            <td>
+                <input type="checkbox" name="hobbies[]" value="Reading"> Reading
+                <input type="checkbox" name="hobbies[]" value="Sports"> Sports
+                <input type="checkbox" name="hobbies[]" value="Music"> Music
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2" align="center">
+                <input type="submit" onclick="goToLogin()" style="cursor: pointer;" value="Login">
+                <input type="submit" value="Register">
+                <input type="reset" value="Clear">
+            </td>
+        </tr>
+    </table>
+</form>
+
 </body>
 </html>
