6393b5ec18cb5a86ac0f1f0fce52992ae4ba5464
MD RAKIB CHOWDHURY <135139732+rakibCHD@users.noreply.github.com>
2025-04-25 01:44:01 +0600

    Update and rename login.php to Login.php

--- Login.php
+++ Login.php
@@ -4,12 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BookVault - Login</title>
-    <link rel="stylesheet" href="./CSS/loginStyle.css">
+    <link rel="stylesheet" href="./CSS/LoginStyle.css">
 </head>
 <body>
     <div class="login-container">
         <header class="header">
-        <h2>BookVault Login</h2>
+            <h2>BookVault Login</h2>
         </header>
 
         <form action="#" method="POST" class="login-form">
@@ -23,12 +23,19 @@
                     <td><input type="password" id="password" name="password" placeholder="Enter your password" required></td>
                 </tr>
                 <tr>
-                    <td colspan="2" style="text-align: center;">
-                        <button type="submit" class="login-button">Login</button>
+                    <td colspan="2">
+                        <div class="button-container">
+                            <button type="submit" class="login-button">Login</button>
+                            <button type="submit" class="forget-password-button">Forget Password</button>
+                        </div>
                     </td>
                 </tr>
             </table>
         </form>
+
+        <div class="register">
+            <h3 class="register-heading">Register</h3>
+        </div>
     </div>
 </body>
 </html>
