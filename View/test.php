8fcbd1edce4b5541d353cc9a49eafd83ebec79f6
Jubayer Ahmed <150487938+jubayerAhmd@users.noreply.github.com>
2025-05-25 10:02:54 +0600

    Add files via upload

---
+++ View/test.php
@@ -0,0 +1,10 @@
+<?php
+
+// Display message..
+    echo "Bookvault is Running using PHP.1.1\n";
+    echo "Bookvault is Running using PHP.2.1\n";
+    echo "Bookvault is Running using PHP.3.1\n";
+    echo "Bookvault is Running using PHP.-.-\n";
+    echo "Bookvault is Running using PHP.*.*\n";
+
+?>
